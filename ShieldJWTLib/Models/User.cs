8866eaf27afd3de72878bb8b84d1e8ef4bbe40cd
igorbock <igorbdaluz@hotmail.com>
2025-03-18 01:57:35 -0300

    Adicionando os atributos de data annotation nas propriedades das classes.

--- ShieldJWTLib/Models/User.cs
+++ ShieldJWTLib/Models/User.cs
@@ -1,11 +1,24 @@
-﻿namespace ShieldJWTLib.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShieldJWTLib.Models
 {
+    [Table("usuario")]
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        [EmailAddress]
+        [StringLength(50)]
         public string Email { get; set; }
+
+        [StringLength(20)]
         public string Username { get; set; }
+
+        [StringLength(500)]
         public string Hash { get; set; }
+
         public bool EmailConfirmed { get; set; }
     }
 }
