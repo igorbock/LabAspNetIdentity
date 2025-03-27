6024ad7fa3994be96c2f394bb79bce301cf183ec
igorbock <igorbdaluz@hotmail.com>
2025-03-27 00:29:58 -0300

    Ajustes no YAML do Actions e exibição de ambiente no TesteController.

--- ShieldJWT/Controllers/TesteController.cs
+++ ShieldJWT/Controllers/TesteController.cs
@@ -4,9 +4,16 @@
 [Route("[controller]")]
 public class TesteController : ControllerBase
 {
+    private readonly IConfiguration _configuration;
+
+    public TesteController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     [HttpGet]
     public IActionResult Index()
     {
-        return Ok();
+        return Ok(_configuration["Environment"]);
     }
 }
