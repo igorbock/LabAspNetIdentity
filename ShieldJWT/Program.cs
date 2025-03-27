f06c49548be9a51b17b2679f3623ba9506a75de6
igorbock <igorbdaluz@hotmail.com>
2025-03-27 01:39:23 -0300

    Modificações para tratamento do appsettings.json

--- ShieldJWT/Program.cs
+++ ShieldJWT/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 var configuration = new ConfigurationBuilder()
     .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
     .Build();
 
