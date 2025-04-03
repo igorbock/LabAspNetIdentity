40a263895d9600df0fca61f2cb7cb795c3cfb69c
igorbdaluz <igorbdaluz@hotmail.com>
2025-04-02 21:48:17 -0300

    Criando a classe de empresa. #17

---
+++ ShieldJWTLib/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShieldJWTLib.Models
+{
+    [Table("company")]
+    public class Company
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [StringLength(80)]
+        public string RazaoSocial { get; set; }
+
+        [StringLength(14)]
+        public string CNPJ { get; set; }
+    }
+}
