79644af532971af3e0ddcdfa151995688acd4086
igorbdaluz <igorbdaluz@hotmail.com>
2025-04-21 11:04:46 -0300

    Subindo as migrations para a tabela de claims. #20

--- ShieldJWT/Migrations/20250421140202_ClaimMigration.Designer.cs
+++ ShieldJWT/Migrations/20250421140202_ClaimMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShieldJWT.Context;
 
@@ -11,9 +12,11 @@ using ShieldJWT.Context;
 namespace ShieldJWT.Migrations
 {
     [DbContext(typeof(ShieldDbContext))]
-    partial class ShieldDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250421140202_ClaimMigration")]
+    partial class ClaimMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ShieldJWT/Migrations/20250421140202_ClaimMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShieldJWT.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClaimMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "claim",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdUser = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_claim", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_claim_user_IdUser",
+                        column: x => x.IdUser,
+                        principalTable: "user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_claim_IdUser",
+                table: "claim",
+                column: "IdUser");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "claim");
+        }
+    }
+}
